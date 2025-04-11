a9c1a1bb42d6e3a16e55a853c2fcf8c89d725fc7
Halil Efecan <hefecan@gmail.com>
2025-04-11 01:27:00 +0100

    setup scripts

---
+++ scripts/portainer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This script runs INSIDE the Multipass VM
+multipass exec single -- bash << 'END'
+    docker volume create portainer_data
+
+    docker run -d \
+    -p 8000:8000 \
+    -p 9443:9443 \
+    --name portainer \
+    --restart=always \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v portainer_data:/data portainer/portainer-ce:lts
+
+    # Login to GUI via VM IP
+    # https://<your-vm-ip>:9443/
+    # Set user credentials
+END
