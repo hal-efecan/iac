2daaf1d04aefbd0362146be89eb24009ab923ea8
Halil Efecan <hefecan@gmail.com>
2025-04-11 14:51:38 +0100

    increased disk space

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,8 +2,8 @@
 
 for vm in my-vm1 my-vm2; do
     if ! multipass info $vm &> /dev/null; then
-        multipass launch --name $vm --cpus 1 --memory 2G --disk 3G
-        multipass launch --name $vm --cpus 1 --memory 2G --disk 3G
+        multipass launch --name $vm --cpus 1 --memory 2G --disk 4G
+        multipass launch --name $vm --cpus 1 --memory 2G --disk 4G
     else
     echo "VM '$vm' already exists."
     fi
