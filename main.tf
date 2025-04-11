9d49ac483dfb709b04c1a2bd4f7e2398e836d89e
Halil Efecan <hefecan@gmail.com>
2025-04-11 01:27:42 +0100

    refactor

--- main.tf
+++ main.tf
@@ -1,10 +1,19 @@
 resource "null_resource" "multipass_vm" {
   provisioner "local-exec" {
-    command = "bash ./single.sh"
+    command = <<-EOT
+      set -e
+      echo "Launching Multipass VM..."
+      bash ./scripts/single.sh
+      echo "Installing Docker inside VM..."
+      bash ./scripts/docker.sh
+      echo "Installing Portianer inside VM..."
+      bash ./scripts/portainer.sh
+      echo "Setup complete!"
+    EOT
+    interpreter = ["/bin/bash", "-c"]
   }
 
   triggers = {
-    always_run = timestamp()
+    script_hash = "${filemd5("./scripts/single.sh")}${filemd5("./scripts/docker.sh")}${filemd5("./scripts/portainer.sh")}"
   }
 }
-
--- single.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-multipass launch --name single --cpus 1 --memory 2G --disk 4G
